6a9f0a24be922dac78b30a6999e7747d125c957b
Prosanto7 <prosantodeb7@gmail.com>
2023-07-20 19:43:30 +0600

    Added more functionality to randomize

--- js/script.js
+++ js/script.js
@@ -5,11 +5,17 @@ function getRandomInteger(min, max) {
   }
 
 function showText() {
-    let randomValue = getRandomInteger(30, 70);
+    let randomValue = getRandomInteger(25, 75);
     let margin = randomValue + "%";
-    document.getElementsByClassName('text-box')[0].style.top = margin;
-    document.getElementsByClassName('text-box')[0].style.left = margin;
-    console.log("Printed");
+    
+    if (randomValue % 2 == 0) {
+      document.getElementsByClassName('text-box')[0].style.top = margin;
+      document.getElementsByClassName('text-box')[0].style.left = margin;
+    } else {
+      document.getElementsByClassName('text-box')[0].style.bottom = margin;
+      document.getElementsByClassName('text-box')[0].style.right = margin;
+    }
+   
 }
 
 setInterval(showText, 1000);
